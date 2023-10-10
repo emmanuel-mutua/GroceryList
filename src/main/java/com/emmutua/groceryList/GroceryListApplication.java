2788b35220c07d87ff681602e9a560289b8d15aa
emmanuel-mutua <emmanuel-mutua>
2023-10-10 16:12:33 +0300

    initial commit

---
+++ src/main/java/com/emmutua/groceryList/GroceryListApplication.java
@@ -0,0 +1,14 @@
+package com.emmutua.groceryList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class GroceryListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryListApplication.class, args);
+	}
+}
---
+++ src/main/java/com/emmutua/groceryList/model/GroceryItem.java
@@ -0,0 +1,8 @@
+package com.emmutua.groceryList.model;
+
+import java.lang.annotation.Documented;
+
+//a pojo
+@Document("groceryitems")
+public class GroceryItem {
+}
